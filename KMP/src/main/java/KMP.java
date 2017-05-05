91b42c238748868717a21fc0835bc4447add0933
huangyichun <576305972@qq.com>
2017-05-05 14:05:12 +0800

    KMP算法实现

---
+++ KMP/src/main/java/KMP.java
@@ -0,0 +1,59 @@
+import org.junit.Test;
+
+/**
+ * Created by huangyichun on 2017/5/5.
+ */
+public class KMP {
+
+    @Test
+    public void test(){
+        String pattern = "ABCDABD";
+        String str = "BBC ABCDAB ABCDABCDABDE";
+        boolean isMatcher = KMP(str,pattern);
+        System.out.println(isMatcher);
+    }
+    //移动位数 = 已匹配的字符数 - 对应的部分匹配值
+    public boolean KMP(String str, String pattern){
+        if(str == null || pattern == null)
+            return false;
+        int[] next = new int[pattern.length()];
+        setNext(pattern, next);
+
+        for(int i=0; i<str.length(); ++i){
+            int index = 0;
+            while (index < pattern.length() && i < str.length()
+                    && str.charAt(i) == pattern.charAt(index)){
+                ++i;
+                ++index;
+            }
+            if(index == pattern.length())
+                return true;
+            if(index != 0){//移动位数
+                i += index - next[index-1];
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 设置next移动具体
+     * @param pattern
+     * @param next
+     */
+    public void setNext(String pattern ,int[] next){
+       int i =1;
+        while(i<next.length){
+            int index = 0;
+            while(i < next.length && pattern.charAt(i) == pattern.charAt(index)){
+                if(index == 0)
+                    next[i] = 1;
+                else
+                     next[i] = next[i-1] + 1;
+                ++i;
+                ++index;
+            }
+            if(index == 0)
+                ++i;
+        }
+    }
+}
