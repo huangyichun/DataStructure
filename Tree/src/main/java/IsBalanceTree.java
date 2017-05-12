7b5f2be4600a362926498f0a6a8a6701ebf03156
huangyichun <576305972@qq.com>
2017-05-12 20:08:31 +0800

    判断二叉树是否平衡

--- Tree/src/main/java/IsBalanceTree.java
+++ Tree/src/main/java/IsBalanceTree.java
@@ -62,11 +62,4 @@ public class IsBalanceTree {
 
 
 
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
-    public TreeNode(int val) {
-        this.val = val;
-    }
-}
+
