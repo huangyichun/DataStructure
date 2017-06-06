782069087d8b09969352478dc1c437c71480ae21
huangyichun <576305972@qq.com>
2017-06-06 10:51:31 +0800

    非递归后序遍历二叉树

---
+++ LeedCode/src/main/java/LeetCode_6.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * For example:
+ * Given binary tree{1,#,2,3},
+ * return[3,2,1].
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ * 非递归遍历后序遍历二叉树
+ */
+public class LeetCode_6 {
+
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null)
+            return list;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode peek = stack.peek();
+            if (peek.left == null && peek.right == null) {
+                list.add(stack.pop().val);
+            }else {
+                if (peek.right != null) {
+                    stack.push(peek.right);
+                    peek.right = null;
+                }
+                if (peek.left != null) {
+                    stack.push(peek.left);
+                    peek.left = null;
+                }
+            }
+        }
+        return list;
+    }
+}
+
