d315629db7bdebc2ee7a77a1186790893a1af852
huangyichun <576305972@qq.com>
2017-06-06 11:25:44 +0800

    判断是否是循环链表

---
+++ LeedCode/src/main/java/LeetCode_10.java
@@ -0,0 +1,19 @@
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+public class LeetCode_10 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode quick = head;
+        do {
+            if (quick == null || quick.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            quick = quick.next.next;
+        }while (quick != slow);
+        return true;
+    }
+}
