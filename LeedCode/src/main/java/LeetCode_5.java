dfceda8c2a1f831cccc2dbf85016a1e354e17557
huangyichun <576305972@qq.com>
2017-06-06 09:49:04 +0800

    链表插入排序

---
+++ LeedCode/src/main/java/LeetCode_5.java
@@ -0,0 +1,28 @@
+/**
+ * Sort a linked list using insertion sort.
+ */
+public class LeetCode_5 {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pedSort = head.next;
+        head.next = null;
+        ListNode node = new ListNode(Integer.MIN_VALUE);
+        node.next = head;
+        head = node;
+        while (pedSort != null){
+            ListNode pre = head;
+            ListNode next = pre.next;
+            while (next != null && next.val <= pedSort.val) {
+                pre = next;
+                next = next.next;
+            }
+            ListNode temp = pedSort;
+            pedSort = pedSort.next;
+            temp.next = pre.next;
+            pre.next = temp;
+        }
+        return head.next;
+    }
+}
