fed8076648ef526c9118ed923aebe99efd6f3409
huangyichun <576305972@qq.com>
2017-06-07 11:28:25 +0800

    gas问题

---
+++ LeedCode/src/main/java/LeetCode_17.java
@@ -0,0 +1,25 @@
+/**
+ * 环形路线上有N个加油站，每个加油站有汽油gas[i]，从每个加油站到下一站消耗汽油cost[i]，
+ * 问从哪个加油站出发能够回到起始点，如果都不能则返回-1（注意，解是唯一的）。
+ */
+public class LeetCode_17 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if(gas == null || cost == null || (gas.length != cost.length)) {
+            throw new IllegalArgumentException("参数错误");
+        }
+        int supTotal = 0, conTotal = 0, diff = 0, index = 0;
+        for (int i = 0; i < gas.length; ++i) {
+            if(diff < 0) {
+                index = i;
+                diff = gas[i] - cost[i];
+            }else {
+                diff += gas[i] - cost[i];
+            }
+           supTotal += gas[i];
+           conTotal += cost[i];
+        }
+        if(supTotal < conTotal)
+            return -1;
+        return index;
+    }
+}
