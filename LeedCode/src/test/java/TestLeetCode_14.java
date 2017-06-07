571133c4e119abe0cfba6d79cf89241afd5a66a5
huangyichun <576305972@qq.com>
2017-06-07 10:48:22 +0800

    Single Number测试

---
+++ LeedCode/src/test/java/TestLeetCode_14.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by huangyichun on 2017/6/7.
+ */
+public class TestLeetCode_14 {
+
+    @Test
+    public void test(){
+        LeetCode_14 leetCode14 = new LeetCode_14();
+        leetCode14.singleNumber(new int[6]);
+    }
+}
