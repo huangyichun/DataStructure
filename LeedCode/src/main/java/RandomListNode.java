3214b5a4bd3a229b5875dede62be309de21739c7
huangyichun <576305972@qq.com>
2017-06-07 09:09:59 +0800

    随机结点，用于复杂链表复制

---
+++ LeedCode/src/main/java/RandomListNode.java
@@ -0,0 +1,12 @@
+/**
+ * Created by huangyichun on 2017/6/7.
+ */
+public class RandomListNode {
+
+    int label;
+    RandomListNode next, random;
+
+    RandomListNode(int x) {
+        this.label = x;
+    }
+}
