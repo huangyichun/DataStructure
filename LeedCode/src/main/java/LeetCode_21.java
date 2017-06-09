ae73a842859ce34919ab1171f65f53efa0da4c19
huangyichun <576305972@qq.com>
2017-06-09 10:22:52 +0800

    添加部分注释

--- LeedCode/src/main/java/LeetCode_21.java
+++ LeedCode/src/main/java/LeetCode_21.java
@@ -16,10 +16,15 @@ import java.util.List;
  * X X X X
  * X X X X
  * X O X X
+ *
+ * 思路:从矩阵的四个边查找字符为’O'的结点
+ * 然后从该节点出发使用广度优先搜素，将找到的节点改为'F'
  */
 public class LeetCode_21 {
 
-    static final int[][] offset = new int[][]{{-1, 0},{0, -1},{1, 0},{0, 1}};
+    private static final int[][] offset = new int[][]{{-1, 0},{0, -1},{1, 0},{0, 1}};
+    private static final int FREE = 'F';
+    private static final int TRAVEL = 'T';
     public void solve(char[][] board) {
         if(board == null || board.length <= 1 || board[0].length <= 1){
             return;
@@ -29,26 +34,25 @@ public class LeetCode_21 {
         //第一行和最后一行
         for (int i = 0; i < col; ++i) {
             if(board[0][i] == 'O'){
-                changeOToY(board, new Point(0, i));
+                bfs(board, new Point(0, i));
             }
             if(board[row-1][i] == 'O'){
-                changeOToY(board, new Point(row-1, i));
+                bfs(board, new Point(row-1, i));
             }
         }
         //第一列和最后一列
         for (int i = 1; i < row-1; ++i) {
             if(board[i][0] == 'O'){
-                changeOToY(board, new Point(i, 0));
+                bfs(board, new Point(i, 0));
             }
             if(board[i][col-1] == 'O'){
-                changeOToY(board,  new Point(i, col-1));
+                bfs(board,  new Point(i, col-1));
             }
         }
 
-
         for(int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[0].length; ++j) {
-                if(board[i][j] == '*') {
+                if(board[i][j] == TRAVEL) {
                     board[i][j] = 'O';
                 }else if(board[i][j] == 'O'){
                     board[i][j] = 'X';
@@ -56,30 +60,30 @@ public class LeetCode_21 {
             }
         }
     }
-   /* public void changeOToY(char[][] board, int x, int y) {
-        for (int i = 0; i < offset.length; ++i) {
-            int offsetX = x + offset[i][0];
-            int offsetY = y + offset[i][1];
-            if (offsetX >= 0 && offsetX < board.length &&
-                    offsetY>=0 && offsetY <board[0].length && board[offsetX][offsetY] == 'O') {
-                board[offsetX][offsetY] = '*';
-                changeOToY(board, offsetX, offsetY);
-            }
-        }
-    }*/
 
-    public void changeOToY(char[][] board, Point point) {
+    /**
+     * 采用广度优先搜素
+     * @param board
+     * @param point
+     */
+    public void bfs(char[][] board, Point point) {
         LinkedList<Point> queue = new LinkedList<>();
         queue.offer(point);
         while (!queue.isEmpty()) {
             Point node = queue.poll();
-            board[node.x][node.y] = '*';
+            board[node.x][node.y] = TRAVEL; //一定要修改，不然会重复添加
             for (Point point1 : expand(board, node)) {
                 queue.offer(point1);
             }
         }
     }
 
+    /**
+     * 找到相邻结点为‘O'的结点
+     * @param board
+     * @param node
+     * @return
+     */
     public List<Point> expand(char[][] board, Point node) {
         List<Point> list = new ArrayList<>();
         for(int i = 0; i < offset.length; ++i) {
@@ -88,16 +92,16 @@ public class LeetCode_21 {
 
             if (offsetX >= 0 && offsetX < board.length &&
                     offsetY>=0 && offsetY <board[0].length && board[offsetX][offsetY] == 'O') {
-                board[offsetX][offsetY] = 'F';
+                board[offsetX][offsetY] = FREE;
                 list.add(new Point(offsetX, offsetY));
             }
         }
         return list;
     }
+
     class Point {
         int x;
         int y;
-
         Point(int a, int b) {
             x = a;
             y = b;
