8f487f25a3d06195e9720805060a791696e09a1f
huangyichun <576305972@qq.com>
2017-07-04 15:27:08 +0800

    判断二叉树是否是镜像

---
+++ LeedCode/src/main/java/SymmetricTree.java
@@ -0,0 +1,23 @@
+/**
+ * 判断二叉树是否是自身的镜像
+ */
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+
+        if(root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null && right != null || (left != null && right == null) || (left.val != right.val)) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
